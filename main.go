b0e31f4d1863783152f2137088ee46bbd6fa4f3b
Abhiram <abhiram.elf@gmail.com>
2021-05-26 22:27:36 +0530

    Simple page serving

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", indexPage)
+	http.ListenAndServe(":8080", nil)
+}
+
+func indexPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Welcome to Abhiram's site!</h1>")
 }
