cea945fbaeeba3c32a36044a2bf79cddf2ba48f6
Abhiram <abhiram.elf@gmail.com>
2021-06-01 22:54:16 +0530

    Added contact page

--- main.go
+++ main.go
@@ -7,10 +7,16 @@ import (
 
 func main() {
 	http.HandleFunc("/", indexPage)
+	http.HandleFunc("/contact", contactPage)
 	http.ListenAndServe(":8080", nil)
 }
 
 func indexPage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to Abhiram's site!</h1>")
+}
+
+func contactPage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "Email us at <a href=\"mailto:mail@odyn.com\">mail@odyn.com</a>.")
 }
